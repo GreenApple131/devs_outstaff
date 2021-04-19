54246ba6c4543d3b31c7a81b11620894cead9359
GreenApple131 <dimalg400@gmail.com>
2021-04-20 00:24:53 +0300

    Revert "add dockerfile + update requirements"
    This reverts commit fbc3bcb1b4b1acfda68e1b14b7d0652c93ec80f9.

--- django_train/account/tests.py
+++ django_train/account/tests.py
@@ -1,14 +1,3 @@
 from django.test import TestCase
-from django.contrib.auth.models import User
+
 # Create your tests here.
-
-
-class UserTestCase(TestCase):
-    def test_user(self):
-        username = 'testcase'
-        password = 'hellopass'
-        u = User(username=username)
-        u.save_password(password)
-        u.save()
-        self.assertEqual(u.username, username)
-        self.assertTrue(u.check_password(password))
--- django_train/django_train/settings.py
+++ django_train/django_train/settings.py
@@ -76,45 +76,12 @@ WSGI_APPLICATION = 'django_train.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-
-if os.getenv('GITHUB_WORKFLOW'):
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
-        }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
-else:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
-        }
-    }
-
-
-# if os.getenv('GITHUB_WORKFLOW'):
-#     DATABASES = {
-#         'default': {
-#             'ENGINE': 'django.db.backends.postgresql',
-#             'NAME': 'github-actions',
-#             'USER': 'postgres',
-#             'PASSWORD': 'postgres',
-#             'HOST': 'localhost',
-#             'PORT': '5432'
-#         }
-#     }
-# else:
-#     DATABASES = {
-#         'default': {
-#             'ENGINE': 'django.db.backends.postgresql',
-#             'NAME': os.getenv('DB_NAME'),
-#             'USER': os.getenv('DB_USER'),
-#             'PASSWORD': os.getenv('DB_PASSWORD'),
-#             'HOST': os.getenv('DB_HOST'),
-#             'PORT': os.getenv('DB_PORT')
-#         }
-#     }
+}
 
 
 # Password validation
